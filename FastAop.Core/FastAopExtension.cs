d3e18c1778bbce69c763025b725c4e66c5af17ad
weizhonzhen <czdswzz@126.com>
2021-11-09 17:25:10 +0800

    Add files via upload

--- FastAop.Core/FastAopExtension.cs
+++ FastAop.Core/FastAopExtension.cs
@@ -3,7 +3,7 @@ using System;
 using System.Linq;
 using System.Reflection;
 
-namespace FastAop.Core
+namespace Microsoft.Extensions.DependencyInjection
 {
     public static class FastAopExtension
     {
