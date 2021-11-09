f4bf5609921d2da91331913cbfad854b55951232
weizhonzhen <czdswzz@126.com>
2021-11-09 17:07:53 +0800

    Add files via upload

---
+++ FastAop.Test/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using FastAop;
+
+namespace FastAop.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)//CreateDelegate
+        {
+           //var model = FastAop.Instance<TestAop, ITestAop>();
+            var model = (ITestAop)FastAop.Instance(typeof(TestAop), typeof(ITestAop));
+
+            model.Test1("1", "3");
+            model.Test1("2", "4");
+            Console.Read();
+        }
+    }
+
+    public class TestAop : ITestAop
+    {
+        [Log1Aop(Sort = 1)]
+        [LogAop(Sort = 2)]
+        public string Test1(string a,string b)
+        {
+            int ae = 0;
+            a += "_b";
+           //var ad = 9 / ae;
+            return a;
+        }
+    }
+
+    public interface ITestAop
+    {
+        string Test1(string a, string b);
+    }
+
+
+    public class LogAop : FastAopAttribute
+    {
+        public override void After(AfterContext context)
+        {
+            //throw new NotImplementedException();
+        }
+
+
+        public override void Before(BeforeContext context)
+        {
+            //throw new NotImplementedException();
+
+        }
+
+        public override void Exception(ExceptionContext exception)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+
+    public class Log1Aop : FastAopAttribute
+    {
+        public override void After(AfterContext context)
+        {
+            //throw new NotImplementedException();
+        }
+
+
+        public override void Before(BeforeContext context)
+        {
+            //throw new NotImplementedException();
+
+        }
+
+        public override void Exception(ExceptionContext exception)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
