607d4025915e26ffa1b502bd3c55c57195a70ab8
weizhonzhen <czdswzz@126.com>
2022-02-08 08:57:15 +0800

    Add files via upload

--- FastAop.Core/FastAopExtension.cs
+++ FastAop.Core/FastAopExtension.cs
@@ -45,6 +45,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 });
             }
 
+            serviceProvider = serviceCollection.BuildServiceProvider();
             return serviceCollection;
         }
 
