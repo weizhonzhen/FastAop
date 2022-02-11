946f40488e90dae58f4095203c464547d43ab50e
weizhonzhen <czdswzz@126.com>
2022-02-11 21:04:18 +0800

    Add files via upload

--- FastAop.Core/FastAop.cs
+++ FastAop.Core/FastAop.cs
@@ -180,9 +180,8 @@ namespace FastAop.Core
                 var afterMethod = aopAttrType.GetMethod("After");
                 var exceptionMethod = aopAttrType.GetMethod("Exception");
 
-                if (attrType == null)
-                    aopAttribute = serviceType.GetMethod(currentMthod.Name, mTypes).GetCustomAttributes().Where(d => aopAttrType.IsAssignableFrom(d.GetType())).Cast<FastAopAttribute>().OrderBy(d => d.Sort).ToList();
-                else
+                aopAttribute = serviceType.GetMethod(currentMthod.Name, mTypes).GetCustomAttributes().Where(d => aopAttrType.IsAssignableFrom(d.GetType())).Cast<FastAopAttribute>().OrderBy(d => d.Sort).ToList();
+                if (attrType != null)
                 {
                     //auto add FastAopAttribute
                     var classCtorInfo = attrType.GetConstructor(Type.EmptyTypes);
@@ -506,9 +505,8 @@ namespace FastAop.Core
                 var afterMethod = aopAttrType.GetMethod("After");
                 var exceptionMethod = aopAttrType.GetMethod("Exception");
 
-                if (attrType == null)
-                    aopAttribute = serviceType.GetMethod(currentMthod.Name, mTypes).GetCustomAttributes().Where(d => aopAttrType.IsAssignableFrom(d.GetType())).Cast<FastAopAttribute>().OrderBy(d => d.Sort).ToList();
-                else
+                aopAttribute = serviceType.GetMethod(currentMthod.Name, mTypes).GetCustomAttributes().Where(d => aopAttrType.IsAssignableFrom(d.GetType())).Cast<FastAopAttribute>().OrderBy(d => d.Sort).ToList();
+                if (attrType != null)
                 {
                     //auto add FastAopAttribute
                     var classCtorInfo = attrType.GetConstructor(Type.EmptyTypes);
