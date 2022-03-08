7ab1f5c6e1f557755d9bdb036606396fc06b26bd
weizhonzhen <czdswzz@126.com>
2022-03-08 11:13:35 +0800

    Add files via upload

--- FastAop/FastAop.cs
+++ FastAop/FastAop.cs
@@ -763,6 +763,22 @@ namespace FastAop
                 return null;
         }
     }
+
+    internal class FastAopCache
+    {
+        private static Dictionary<string, Type> cache = new Dictionary<string, Type>();
+
+        public static Type GetType(string key)
+        {
+            if (cache.ContainsKey(key))
+                return cache[key];
+            else
+            {
+                cache.Add(key, Type.GetType(key));
+                return cache[key];
+            }
+        }
+    }
 }
 
 
