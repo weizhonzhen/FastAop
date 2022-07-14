d44f0f03a5f891844cf7f5750e25712fc62f739e
weizhonzhen <czdswzz@126.com>
2022-07-14 11:21:37 +0800

    Add files via upload

--- FastAop/Model/ConstructorModel.cs
+++ FastAop/Model/ConstructorModel.cs
@@ -1,1 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
 
+namespace FastAop.Model
+{
+    internal class ConstructorModel
+    {
+        public List<object> dynParam { get; set; } = new List<object>();
+
+        public List<Type> constructorType { get; set; } = new List<Type>();
+
+        public List<Type> dynType { get; set; } = new List<Type>();
+
+        public Type serviceType { get; set; }
+
+        public Type interfaceType { get; set; }
+    }
+}
