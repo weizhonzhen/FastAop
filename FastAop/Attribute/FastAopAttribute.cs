c4584d1a391a66c37f5e88198458fec4e99ea697
weizhonzhen <czdswzz@126.com>
2022-07-14 11:17:27 +0800

    Add files via upload

--- FastAop/Attribute/FastAopAttribute.cs
+++ FastAop/Attribute/FastAopAttribute.cs
@@ -1,1 +1,26 @@
+﻿using FastAop.Context;
+using System;
 
+namespace FastAop
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public abstract class FastAopAttribute : Attribute, IFastAop
+    {
+        public short Sort { get; set; } = 0;
+
+        public abstract void Before(BeforeContext context);
+
+        public abstract void After(AfterContext context);
+
+        public abstract void Exception(ExceptionContext exception);
+    }
+
+    internal interface IFastAop
+    {
+        void Before(BeforeContext context);
+
+        void After(AfterContext context);
+
+        void Exception(ExceptionContext exception);
+    }
+}
