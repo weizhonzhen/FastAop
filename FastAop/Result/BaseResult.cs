dedf4ac73a9b41bd7812e943f3fd94b56c7fd74b
weizhonzhen <czdswzz@126.com>
2022-07-14 11:22:28 +0800

    Add files via upload

--- FastAop/Result/BaseResult.cs
+++ FastAop/Result/BaseResult.cs
@@ -1,1 +1,111 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
 
+namespace FastAop.Result
+{
+    internal static class BaseResult
+    {
+        private delegate object EmitInvoke(object target, object[] paramters);
+
+        private static object Invoke(object model, MethodInfo methodInfo, object[] param)
+        {
+            if (methodInfo == null || model == null)
+                return null;
+            try
+            {
+                var dynamicMethod = new DynamicMethod("InvokeEmit", typeof(object), new Type[] { typeof(object), typeof(object[]) }, typeof(EmitInvoke).Module);
+                var iL = dynamicMethod.GetILGenerator();
+                var info = methodInfo.GetParameters();
+                var type = new Type[info.Length];
+                var local = new LocalBuilder[type.Length];
+
+                for (int i = 0; i < info.Length; i++)
+                {
+                    if (info[i].ParameterType.IsByRef)
+                        type[i] = info[i].ParameterType.GetElementType();
+                    else
+                        type[i] = info[i].ParameterType;
+                }
+
+                for (int i = 0; i < type.Length; i++)
+                {
+                    local[i] = iL.DeclareLocal(type[i], true);
+                }
+
+                for (int i = 0; i < type.Length; i++)
+                {
+                    iL.Emit(OpCodes.Ldarg_1);
+                    iL.Emit(OpCodes.Ldc_I4, i);
+                    iL.Emit(OpCodes.Ldelem_Ref);
+                    if (type[i].IsValueType)
+                        iL.Emit(OpCodes.Unbox_Any, type[i]);
+                    else
+                        iL.Emit(OpCodes.Castclass, type[i]);
+                    iL.Emit(OpCodes.Stloc, local[i]);
+                }
+
+                if (!methodInfo.IsStatic)
+                    iL.Emit(OpCodes.Ldarg_0);
+
+                for (int i = 0; i < type.Length; i++)
+                {
+                    if (info[i].ParameterType.IsByRef)
+                        iL.Emit(OpCodes.Ldloca_S, local[i]);
+                    else
+                        iL.Emit(OpCodes.Ldloc, local[i]);
+                }
+
+                if (methodInfo.IsStatic)
+                    iL.EmitCall(OpCodes.Call, methodInfo, null);
+                else
+                    iL.EmitCall(OpCodes.Callvirt, methodInfo, null);
+
+                if (methodInfo.ReturnType == typeof(void))
+                    iL.Emit(OpCodes.Ldnull);
+                else if (methodInfo.ReturnType.IsValueType)
+                    iL.Emit(OpCodes.Box, methodInfo.ReturnType);
+                else
+                    iL.Emit(OpCodes.Castclass, methodInfo.ReturnType);
+
+
+                for (int i = 0; i < type.Length; i++)
+                {
+                    if (info[i].ParameterType.IsByRef)
+                    {
+                        iL.Emit(OpCodes.Ldarg_1);
+                        iL.Emit(OpCodes.Ldc_I4, i);
+                        iL.Emit(OpCodes.Ldloc, local[i]);
+                        if (local[i].LocalType.IsValueType)
+                            iL.Emit(OpCodes.Box, local[i].LocalType);
+                        iL.Emit(OpCodes.Stelem_Ref);
+                    }
+                }
+
+                iL.Emit(OpCodes.Ret);
+                var dyn = dynamicMethod.CreateDelegate(typeof(EmitInvoke)) as EmitInvoke;
+                return dyn(model, param);
+            }
+            catch (Exception ex)
+            {
+                return methodInfo.Invoke(model, param);
+            }
+        }
+
+        internal static object GetTaskResult(object result)
+        {
+            if (result == null)
+                return result;
+
+            if (result is Task)
+            {
+                var method = result.GetType().GetMethods().ToList().Find(a => a.Name == "get_Result");
+                return Invoke(result, method, null);
+            }
+            else
+                return result;
+        }
+    }
+}
