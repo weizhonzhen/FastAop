06896a5e10561e4f9e471a746654cb47adbbeea8
weizhonzhen <czdswzz@126.com>
2022-07-18 09:26:27 +0800

    Add files via upload

--- FastAop.Core/Context/BeforeContext.cs
+++ FastAop.Core/Context/BeforeContext.cs
@@ -39,6 +39,9 @@ namespace FastAop.Core.Context
                 if (!isValueTaskResult && BaseResult.IsValueTask(value.GetType()))
                     value = BaseResult.GetTaskResult(value);
 
+                if (value.GetType() != ResultType && IsReturn)
+                    throw new Exception($"ServiceName:{(Method.DeclaringType != null ? Method.DeclaringType.Name : MethodName)},Method Name:{MethodName},return Type:{ResultType.Name},but aop set result type :{value.GetType().Name}");
+
                 if (!IsTaskResult && !isValueTaskResult)
                     _Result = Convert.ChangeType(value, ResultType);
                 else
