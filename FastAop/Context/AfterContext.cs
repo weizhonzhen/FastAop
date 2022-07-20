6b032c8c932e982e185bda7bc263517fdd10cc3b
weizhonzhen <czdswzz@126.com>
2022-07-20 14:21:39 +0800

    Add files via upload

--- FastAop/Context/AfterContext.cs
+++ FastAop/Context/AfterContext.cs
@@ -39,7 +39,7 @@ namespace FastAop.Context
                 if (!IsTaskResult && value is Task)
                     value = BaseResult.GetTaskResult(value);
 
-                if (value.GetType() != ResultType)
+                if (value != null && value.GetType() != ResultType)
                     throw new Exception($"ServiceName:{(Method.DeclaringType != null ? Method.DeclaringType.Name : MethodName)},Method Name:{MethodName},return Type:{ResultType.Name},but aop set result type :{value.GetType().Name}");
 
                 if (!IsTaskResult)
