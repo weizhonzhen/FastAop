b6ff39532095c09efd69f89786735ef8544ddd64
weizhonzhen <czdswzz@126.com>
2022-07-26 08:47:53 +0800

    Add files via upload

--- FastAop.Core/Context/FastAopContext.cs
+++ FastAop.Core/Context/FastAopContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
 
 namespace FastAop.Core.Context
 {
@@ -6,15 +8,42 @@ namespace FastAop.Core.Context
     {
         public static dynamic ResolveDyn<T>()
         {
-            return FastAopExtension.serviceProvider.GetService(typeof(T));
+            var data = FastAopExtension.serviceProvider?.GetService(typeof(T));
+
+            if (!typeof(T).GetInterfaces().Any() && data != null)
+                return data;
+            else if (!typeof(T).GetInterfaces().Any() && data == null)
+                return FastAopDyn.Instance(typeof(T));
+            else
+                return null;
         }
 
         public static T Resolve<T>()
         {
-            if (typeof(T).IsInterface)
-                return FastAopExtension.serviceProvider.GetService<T>();
+            if (FastAopExtension.serviceProvider == null)
+                return default(T);
+
+            var data = FastAopExtension.serviceProvider.GetService<T>();
+            if (typeof(T).IsInterface && data != null)
+                return data;
+            else if (typeof(T).IsInterface && data == null)
+                return default(T);
+            else if (!typeof(T).IsInterface && !typeof(T).GetInterfaces().Any() && typeof(T).GetConstructors().Length > 0)
+            {
+                var model = Constructor.Constructor.Get(typeof(T), null);
+                return (T)Activator.CreateInstance(typeof(T), model.dynParam.ToArray());
+            }
+            else if (!typeof(T).IsInterface && !typeof(T).GetInterfaces().Any() && typeof(T).GetConstructors().Length == 0)
+                return (T)Activator.CreateInstance(typeof(T));
+            else if (!typeof(T).IsInterface && typeof(T).GetInterfaces().Any() && typeof(T).GetConstructors().Length > 0)
+            {
+                var model = Constructor.Constructor.Get(typeof(T), null);
+                return (T)Activator.CreateInstance(typeof(T), model.dynParam.ToArray());
+            }
+            else if (!typeof(T).IsInterface && typeof(T).GetInterfaces().Any() && typeof(T).GetConstructors().Length == 0)
+                return (T)Activator.CreateInstance(typeof(T));
             else
-                return default;
+                throw new Exception($"can't find {typeof(T).Name} Instance class");
         }
     }
 }
