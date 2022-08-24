ea6b825e77221d08daa9929d94488843e6edb8e6
weizhonzhen <czdswzz@126.com>
2022-08-24 09:54:50 +0800

    Add files via upload

--- FastAop/Factory/AopWebFormModule.cs
+++ FastAop/Factory/AopWebFormModule.cs
@@ -1,8 +1,11 @@
 ﻿using FastAop.Constructor;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 
@@ -10,10 +13,10 @@ namespace FastAop.Factory
 {
     public class AopWebFormModule : IHttpModule
     {
+        private ConcurrentDictionary<Type, List<FieldInfo>> cache = new ConcurrentDictionary<Type, List<FieldInfo>>();
         private HttpApplication application;
 
-        public void Dispose()
-        {  }
+        public void Dispose() { }
 
         public void Init(HttpApplication _application)
         {
@@ -26,32 +29,42 @@ namespace FastAop.Factory
 
         private void PreRequestHandlerExecute(object sender, EventArgs e)
         {
+            var list = new List<FieldInfo>();
             var page = application.Context.CurrentHandler as Page;
 
-            foreach (var item in page.GetType().BaseType.GetRuntimeFields())
+            var type = page.GetType().BaseType;
+            cache.TryGetValue(type, out list);
+
+            if (list == null)
+            {
+                list = type.GetRuntimeFields().ToList();
+                cache.TryAdd(type, list);
+            }
+
+            list.ForEach(item =>
             {
                 if (item.GetCustomAttribute<Autowired>() == null)
-                    continue;
+                    return;
 
                 if (item.FieldType.isSysType())
-                    throw new Exception($"{item.Name} is system type not support");
+                    throw new Exception($"{page.GetType().Name} field {item.Name} is system type not support");
 
                 if (item.FieldType.IsInterface && FastAop._types.GetValue(item.FieldType) == null)
-                    throw new Exception($"{item.FieldType.FullName} not in ServiceCollection");
-                
+                    throw new Exception($"{item.FieldType.Name} not in ServiceCollection");
+
                 if (!item.FieldType.IsInterface && item.FieldType.GetInterfaces().Any() && FastAop._types.GetValue(item.FieldType.GetInterfaces().First()) == null)
                     throw new Exception($"{item.FieldType.GetInterfaces().First().FullName} not in ServiceCollection");
-                
+
                 if (!item.FieldType.IsInterface && Dic.GetValueDyn(item.FieldType) == null)
                     throw new Exception($"{item.FieldType.FullName} not in ServiceCollection");
 
                 if (item.FieldType.IsInterface)
                     item.SetValue(page, FastAop._types.GetValue(item.FieldType));
-                else if (item.FieldType.GetInterfaces().Any())
+                else if (item.FieldType.GetInterfaces().Any()) 
                     item.SetValue(page, FastAop._types.GetValue(item.FieldType.GetInterfaces().First()));
                 else
                     item.SetValue(page, Dic.GetValueDyn(item.FieldType));
-            }
+            });
         }
     }
 }
