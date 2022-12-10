8b46dcb225cb2753bc1d9175d7f673796500b442
weizhonzhen <czdswzz@126.com>
2022-12-10 17:11:56 +0800

    Add files via upload

--- FastAop/Factory/AopMvcFactory.cs
+++ FastAop/Factory/AopMvcFactory.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using FastAop.Constructor;
 using System.Collections.Concurrent;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace FastAop.Factory
 {
@@ -40,7 +41,44 @@ namespace FastAop.Factory
                     item.SetValue(instance, Dic.GetValueDyn(item.FieldType));
             }
 
+            if (!ModelValidatorProviders.Providers.ToList().Exists(a => a.GetType().Name == typeof(FastAopMvcModelValidatorProvider).Name))
+            {
+                ModelValidatorProviders.Providers.Clear();
+                ModelValidatorProviders.Providers.Add(new FastAopMvcModelValidatorProvider());
+                ModelValidatorProviders.Providers.Add(new DataErrorInfoModelValidatorProvider());
+                ModelValidatorProviders.Providers.Add(new ClientDataTypeModelValidatorProvider());
+            }
+
             return (IController)instance;
+        }
+    }
+
+    internal class FastAopMvcModelValidatorProvider : DataAnnotationsModelValidatorProvider
+    {
+        protected override IEnumerable<ModelValidator> GetValidators(ModelMetadata metadata, ControllerContext context, IEnumerable<Attribute> attributes)
+        {
+            attributes.ToList().ForEach(a =>
+            {
+                foreach (var item in a.GetType().GetRuntimeFields())
+                {
+                    if (item.GetCustomAttribute<Autowired>() == null)
+                        continue;
+
+                    if (!item.Attributes.HasFlag(FieldAttributes.InitOnly))
+                        throw new AopException($"{a.GetType().Name} field {item} attribute must readonly");
+
+                    if (item.FieldType.isSysType())
+                        throw new Exception($"{a.GetType().Name} field {item} is system type not support");
+
+                    if (item.FieldType.IsInterface)
+                        item.SetValueDirect(__makeref(a), FastAop.ServiceInstance.GetValue(item.FieldType));
+                    else if (item.FieldType.GetInterfaces().Any())
+                        item.SetValueDirect(__makeref(a), FastAop.ServiceInstance.GetValue(item.FieldType.GetInterfaces().First()));
+                    else
+                        item.SetValue(a, Dic.GetValueDyn(item.FieldType));
+                }
+            });
+            return base.GetValidators(metadata, context, attributes);
         }
     }
 }
--- FastAop/Factory/AopWebApiFactory.cs
+++ FastAop/Factory/AopWebApiFactory.cs
@@ -1,11 +1,16 @@
 ﻿using FastAop.Constructor;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Reflection;
+using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Dispatcher;
+using System.Web.Http.Metadata;
+using System.Web.Http.Validation;
+using System.Web.Http.Validation.Providers;
 
 namespace FastAop.Factory
 {
@@ -44,7 +49,39 @@ namespace FastAop.Factory
                     item.SetValue(instance, Dic.GetValueDyn(item.FieldType));
             }
 
+            if (!controllerDescriptor.Configuration.Services.GetModelValidatorProviders().ToList().Exists(a => a.GetType().Name == typeof(FastAopWebApiModelValidatorProvider).Name))            
+                controllerDescriptor.Configuration.Services.Replace(typeof(ModelValidatorProvider), new FastAopWebApiModelValidatorProvider());
+            
             return (IHttpController)instance;
+        }
+    }
+
+    public class FastAopWebApiModelValidatorProvider : DataAnnotationsModelValidatorProvider
+    {
+        protected override IEnumerable<ModelValidator> GetValidators(ModelMetadata metadata, IEnumerable<ModelValidatorProvider> validatorProviders, IEnumerable<Attribute> attributes)
+        {
+            attributes.ToList().ForEach(a =>
+            {
+                foreach (var item in a.GetType().GetRuntimeFields())
+                {
+                    if (item.GetCustomAttribute<Autowired>() == null)
+                        continue;
+
+                    if (!item.Attributes.HasFlag(FieldAttributes.InitOnly))
+                        throw new AopException($"{a.GetType().Name} field {item} attribute must readonly");
+
+                    if (item.FieldType.isSysType())
+                        throw new Exception($"{a.GetType().Name} field {item} is system type not support");
+
+                    if (item.FieldType.IsInterface)
+                        item.SetValueDirect(__makeref(a), FastAop.ServiceInstance.GetValue(item.FieldType));
+                    else if (item.FieldType.GetInterfaces().Any())
+                        item.SetValueDirect(__makeref(a), FastAop.ServiceInstance.GetValue(item.FieldType.GetInterfaces().First()));
+                    else
+                        item.SetValue(a, Dic.GetValueDyn(item.FieldType));
+                }
+            });
+            return base.GetValidators(metadata, validatorProviders, attributes);
         }
     }
 }
